cd09004388d80b5ba579a8d66b31c32b97d526ad
Guilherme Affonso <guilherme.c.affonso@gmail.com>
2022-05-19 22:51:01 +0900

    Add takePose block on pepper

--- src/extensions/scratch3_pepperrobot/index.js
+++ src/extensions/scratch3_pepperrobot/index.js
@@ -1,6 +1,8 @@
 const ArgumentType = require('../../extension-support/argument-type');
 const BlockType = require('../../extension-support/block-type');
 const Scratch3RobotBase = require('../scratch3_ros/RobotUtil');
+const {RosUtil} = require('../scratch3_ros/RosUtil');
+const ROSLIB = require('roslib');
 const Cast = require('../../util/cast');
 const icon = require('./icon');
 
@@ -10,6 +12,43 @@ class Scratch3PepperRobotBlocks extends Scratch3RobotBase {
         let masterURI = prompt('Master URI:');
         super('Pepper', 'pepperRobot', '/robotsound', '/robotsound_jp', runtime, masterURI);
         this.icon = icon;
+
+        this.joint_server = '/pepper_robot/pose/joint_trajectory';
+        this.joint_names = ['KneePitch', 'HipRoll', 'HipPitch', 'LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll', 'LWristYaw', 'RShoulderPitch', 'RShoulderRoll', 'RElbowYaw', 'RElbowRoll', 'RWristYaw', 'HeadYaw', 'HeadPitch'];
+
+        this.poses = {
+            'nodding-pose': [0.034906585039886584, -0.034906585039886584, -0.08726646259971643, 1.919862177193762, 0.6108652381980151, -0.6981317007977315, -1.0471975511965974, -0.6981317007977315, 1.919862177193762, -0.6108652381980151, 0.6981317007977315, 1.0471975511965974, 0.6981317007977315, 0.0, 0.26179938779914935],
+
+            'pointing-left-pose': [0.034906585039886584, 0.0, -0.08726646259971643, 0.8726646259971642, 1.396263401595463, -1.5707963267948957, -0.34906585039886573, -1.5707963267948957, 1.4835298641951793, -0.17453292519943286, 1.2217304763960302, 0.34906585039886573, 0.6981317007977315, 0.5235987755982987, -0.1047197551196597],
+
+            'pointing-right-pose': [0.034906585039886584, 0.0, -0.08726646259971643, 1.4835298641951793, 0.17453292519943286, -1.2217304763960302, -0.34906585039886573, -0.6981317007977315, 0.8726646259971642, -1.396263401595463, 1.5707963267948957, 0.34906585039886573, 1.5707963267948957, -0.5235987755982987, -0.06981317007977317],
+
+            'reset-pose': [0.034906585039886584, -0.034906585039886584, -0.08726646259971643, 1.4835298641951793, 0.17453292519943286, -1.2217304763960302, -0.34906585039886573, -0.6981317007977315, 1.4835298641951793, -0.17453292519943286, 1.2217304763960302, 0.34906585039886573, 0.6981317007977315, 0.0, 0.0],
+
+            'watching-pose': [0.034906585039886584, -0.034906585039886584, -0.08726646259971643, 1.7453292519943284, 0.8726646259971642, -0.34906585039886573, -1.396263401595463, -0.6981317007977315, -1.0471975511965974, -0.34906585039886573, -0.17453292519943286, 1.0471975511965974, 0.6981317007977315, 0.0, 0.0],
+
+            'wondering-pose': [0.034906585039886584, -0.34906585039886573, -0.08726646259971643, 1.7453292519943284, 0.8726646259971642, -0.34906585039886573, -1.396263401595463, -0.6981317007977315, -1.0471975511965974, -0.34906585039886573, -0.17453292519943286, 1.0471975511965974, 0.6981317007977315, -0.08726646259971643, 0.0],
+
+            'c-pose': [0.034906585039886584, -0.034906585039886584, -0.08726646259971643, 0.0, 1.2217304763960302, -1.2217304763960302, -0.34906585039886573, -0.6981317007977315, -0.6981317007977315, -0.17453292519943286, 0.34906585039886573, 1.2217304763960302, 0.6981317007977315, 0.0, -0.34906585039886573],
+
+            'hiragana-tsu-pose': [0.034906585039886584, -0.034906585039886584, -0.08726646259971643, -0.6981317007977315, 0.17453292519943286, -0.34906585039886573, -1.2217304763960302, -0.6981317007977315, 0.0, -1.2217304763960302, 1.2217304763960302, 0.34906585039886573, 0.6981317007977315, 0.0, -0.34906585039886573],
+
+            'bowing-pose': [0.034906585039886584, -0.034906585039886584, -0.6981317007977315, 1.4835298641951793, 0.17453292519943286, -1.2217304763960302, -0.34906585039886573, -0.6981317007977315, 0.8726646259971642, -0.017453292519943292, -0.017453292519943292, 0.8726646259971642, 0.8726646259971642, 0.0, 0.34906585039886573],
+
+            'ok-pose': [0.034906585039886584, -0.034906585039886584, -0.08726646259971643, -0.6981317007977315, 0.17453292519943286, -0.34906585039886573, -1.2217304763960302, -0.6981317007977315, -0.6981317007977315, -0.17453292519943286, 0.34906585039886573, 1.2217304763960302, 0.6981317007977315, 0.0, -0.34906585039886573],
+
+            'greeting-pose': [0.034906585039886584, -0.034906585039886584, -0.08726646259971643, 1.4835298641951793, 0.17453292519943286, 0.5235987755982987, -0.008726646259971646, 0.5235987755982987, -0.34906585039886573, -0.34906585039886573, 1.2217304763960302, 0.34906585039886573, 0.6981317007977315, 0.0, -0.26179938779914935],
+
+            'init-pose': [0.034906585039886584, -0.034906585039886584, -0.08726646259971643, 1.919862177193762, 0.6108652381980151, -0.6981317007977315, -1.0471975511965974, -0.6981317007977315, 1.919862177193762, -0.6108652381980151, 0.6981317007977315, 1.0471975511965974, 0.6981317007977315, 0.0, 0.26179938779914935]
+        }
+    }
+
+    connect (url) {
+        this.ros = new RosUtil(this.runtime, this.extensionId, {url: url});
+        this.ros.on('connection', () => {
+            console.log("Setting joint server...");
+            this._setJointServer();
+        });
     }
 
     _toRange(val, min, max) {
@@ -18,6 +57,41 @@ class Scratch3PepperRobotBlocks extends Scratch3RobotBase {
 
     _makeModifier(key, value) {
         return "\\\\" + key + "=" + value + "\\\\";
+    }
+
+    _makeJointRequest(positions, time) {
+        if (time < 1) time = 1;
+        let secs = Math.floor(time);
+        let nsecs = parseInt((time-secs) * 1000000000);
+        return {
+            trajectory: {
+                joint_names: this.joint_names,
+                points: [
+                    {
+                        positions: positions,
+                        velocities: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                     0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+                        time_from_start: {
+                            secs: secs,
+                            nsecs: nsecs,
+                        }
+                    } ]
+            }
+        };
+    }
+
+    _setJointServer () {
+        if (typeof this.joint_server == 'string') {
+            this.joint_server = new ROSLIB.ActionClient({
+                ros: this.ros,
+                serverName: this.joint_server,
+                actionName: 'naoqi_bridge_msgs/JointTrajectoryAction'
+            });
+        }
+    }
+
+    _poseNames () {
+        return Object.keys(this.poses).sort();
     }
 
     speech({TEXT}) {
@@ -51,6 +125,16 @@ class Scratch3PepperRobotBlocks extends Scratch3RobotBase {
 
     resetModifier() {
         return ('\\\\rst\\\\');
+    }
+
+    takePose({POSE, TIME, WAIT}) {
+        TIME = Cast.toNumber(TIME);
+        WAIT = Cast.toBoolean(WAIT);
+        let pose = this.poses[POSE];
+        let msg = this._makeJointRequest(pose, TIME);
+        this._setJointServer();
+        return this._waitPromise(this.ros.callActionInstance(this.joint_server, msg, WAIT)).
+            catch(err => this._reportError(err));
     }
 
     getInfo () {
@@ -130,8 +214,32 @@ class Scratch3PepperRobotBlocks extends Scratch3RobotBase {
                     }
                 },
                 '---',
+                {
+                    opcode: 'takePose',
+                    blockType: BlockType.COMMAND,
+                    text: 'pose [POSE] in [TIME] s wait: [WAIT]',
+                    arguments: {
+                        POSE: {
+                            type: ArgumentType.STRING,
+                            menu: 'poseMenu',
+                            defaultValue: this._poseNames()[0]
+                        },
+                        TIME: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 5
+                        },
+                        WAIT: {
+                            type: ArgumentType.BOOLEAN,
+                            menu: 'booleanMenu',
+                            defaultValue: 'true'
+                        }
+                    }
+                },
             ],
-            menus: {}
+            menus: {
+                booleanMenu: ['true', 'false'],
+                poseMenu: '_poseNames'
+            }
         };
     }
 }
